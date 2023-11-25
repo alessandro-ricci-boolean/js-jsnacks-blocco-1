92a65463901215fb5021b24ccbead89d5bb417f5
alessandro-ricci-boolean <cqfp.official@gmail.com>
2023-11-25 22:06:21 +0100

    completed exercise n.1

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// ESERCIZIO 1
+
+const fruits = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
+
+fruits.push("pesca");
+
+let flag = false;
+
+for(let i=0; i<fruits.length; i++){
+    if(fruits[i] == "cocomero"){
+        flag = true;
+    }
+}
+
+if(flag == true){
+    console.log("Trovato! Devo solo preparare il cocktail.");
+}
+else{
+    console.log("Oh no, devo uscire a comprare il cocomero!");
+}
+
+// ESERCIZIO 2
+
