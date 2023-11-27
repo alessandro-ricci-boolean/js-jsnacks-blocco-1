32baaeea3c1d872a26741baef8206a5404fcf05b
alessandro-ricci-boolean <146222580+alessandro-ricci-boolean@users.noreply.github.com>
2023-11-27 17:46:42 +0100

    Update script.js

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -2,11 +2,11 @@ let number = prompt("Inserire il numero di elementi")
 
 let numbers = []
 for(let i=0; i<number; i++){
-    let num = Math.floor((Math.random) * 100) + 1;
+    let num = Math.floor(Math.random() * 100 + 1);
 numbers.push(num);
 }
 console.log(numbers);
 
 numbers.reverse();
-let new_numbers = numbers.slice(0.5);
+let new_numbers = numbers.slice(-5);
 console.log(new_numbers);
